e99f3634c0670f773e11986a476691f06f38dcdd
Safiyat Reza <md.reza@snapdeal.com>
2016-06-28 18:01:29 +0530

    Path expansion for bash done.

--- setup.sh
+++ setup.sh
@@ -3,12 +3,14 @@
 if [ -n "$HOME" ]; then
     path=$HOME/.aria2/aria2.conf
 elif [ -n "$XDG_CONFIG_HOME" ]; then
-    path=$XDG_CONFIG_HOME/aria2/aria2.conf
+    path=$XDG_CONFIG_HOME/.aria2/aria2.conf
 else
     echo "No home path available. Exiting..."
     exit
 fi
 
+home=${path%.aria2/aria2.conf}
+
 mkdir -p $(dirname $path)
 
-cp aria2.conf $path
+envsubst < aria2.conf > $path
